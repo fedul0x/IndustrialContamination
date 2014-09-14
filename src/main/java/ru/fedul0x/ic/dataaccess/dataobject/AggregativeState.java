ce8960cb4ad945f420c7027b008a757434b771c5
Ivashin Alexey <ivashin.alexei@gmail.com>
2014-09-14 23:47:37 +0400

    AggregativeState.java changes

--- src/main/java/ru/fedul0x/ic/dataaccess/dataobject/AggregativeState.java
+++ src/main/java/ru/fedul0x/ic/dataaccess/dataobject/AggregativeState.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Юлия.
+ * Copyright 2014 Ivashin Alexey <ivashin.alexei@gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +17,33 @@ package ru.fedul0x.ic.dataaccess.dataobject;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import ru.fedul0x.ic.dataaccess.DataEntity;
+import ru.fedul0x.ic.dataaccess.TitledColumn;
 
 /**
+ * Aggregative state of some contamination
+ * <ul>
+ * <li>твердый</li>
+ * <li>жидкий</li>
+ * <li>постообразный</li>
+ * <li>шлам</li>
+ * <li>гель</li>
+ * <li>эмульсия</li>
+ * <li>суспензия</li>
+ * <li>сыпучий</li>
+ * <li>гранулят</li>
+ * <li>порошкообразный</li>
+ * <li>пылеобразный</li>
+ * <li>волокно</li>
+ * <li>готовое изделие, потерявшее свои потребительские свойства</li>
+ * <li>иное</li>
+ * </ul>
  *
- * @author Юлия
+ * @author Ivashin Alexey <ivashin.alexei@gmail.com>
  */
 @Entity
 @Table(name = "aggregative_state", schema = "public")
@@ -39,7 +59,8 @@ public class AggregativeState extends DataEntity {
     }
 
     @Id
-    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @TitledColumn(name = "id", title="Агрегатное состояние", unique = true, nullable = false)
     @Override
     public Long getId() {
         return this.id;
